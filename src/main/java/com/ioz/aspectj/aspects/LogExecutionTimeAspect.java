0f0478954b1a1574b46e981d7675489e43b01ce6
Tibor Kiss <trks1970@gmail.com>
2019-03-10 23:15:32 +0100

    LogExecutionTime

--- src/main/java/com/ioz/aspectj/aspects/LogExecutionTimeAspect.java
+++ src/main/java/com/ioz/aspectj/aspects/LogExecutionTimeAspect.java
@@ -3,18 +3,19 @@ package com.ioz.aspectj.aspects;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.SuppressAjWarnings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.ioz.aspectj.annotations.LogExecutionTime;
 
 @Aspect
 public class LogExecutionTimeAspect
 {
 	private static final Logger STAT = LoggerFactory.getLogger( "execution.STATS" );
 
-	@Around("execution(* *(..)) && @annotation(com.ioz.aspectj.annotations.LogExecutionTime) && args(logExecutionTime)")
-	public Object logExecutionTime( ProceedingJoinPoint joinPoint, LogExecutionTime logExecutionTime ) throws Throwable
+	//@Around("execution(* *(..)) && @annotation(com.ioz.aspectj.annotations.LogExecutionTime) && args(logExecutionTime)")
+	@SuppressAjWarnings("adviceDidNotMatch")
+	@Around("execution(* *(..)) && @annotation(com.ioz.aspectj.annotations.LogExecutionTime)")
+	public Object logExecutionTime( ProceedingJoinPoint joinPoint/*, LogExecutionTime logExecutionTime*/ ) throws Throwable
 	{
 		final long start = System.currentTimeMillis();
 
@@ -22,13 +23,12 @@ public class LogExecutionTimeAspect
 		
 		final long executionTime = System.currentTimeMillis() - start;
 		
-		Logger log = STAT;
+		/*Logger log = STAT;
 		if( logExecutionTime.useClassLogger() )
 		{
 			log = LoggerFactory.getLogger( joinPoint.getTarget().getClass() );
-		}
-		log.info( joinPoint.getSignature() + " executed in " + executionTime + "ms" );
-
+		}*/
+		STAT.info( joinPoint.getSignature() + " executed in " + executionTime + "ms" );
 		return proceed;
 	}
 
